4287223cea6337e8779e507fa8e3bdccee1b7e5b
Carlos Henrique <CarlxsHenriqueLS@gmail.com>
2023-12-05 17:08:48 -0300

    estudo js 05-12

---
+++ html-css/05-12/app.js
@@ -0,0 +1,38 @@
+// Soma
+function soma(a, b) {
+    return a + b;
+  }
+
+let som = soma(10, 11)
+console.log(`O resultado da subtração é: ${som}`)
+
+
+// Subtração
+
+function subtracao(a, b) {
+    return a - b;
+  }
+
+let sub = subtracao(8, 11)
+console.log(`O resultado da subtração é: ${sub}`)
+
+
+// Multiplicação
+function multiplicar(a, b) {
+    return a * b;
+  }
+
+let mult = multiplicar(1, 2)
+console.log(`O resultado da multiplicação é: ${mult}`)
+
+
+// Divisão
+function dividir(a, b) {
+    return a / b;
+  }
+
+let divs = dividir(5, 3).toFixed(2)
+console.log(`O resultado da multiplicação é: ${divs}`)
+
+
+  
